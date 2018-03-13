c668473e6e03efbba205d869c8d453a654d36646
Nick Chan <nickchan@hk01.com>
2018-03-14 07:26:25 +0800

    add webview event examples

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -1,11 +1,67 @@
+import {
+  EVENT_ENUMS,
+  isSupportedApp,
+  requestAndWait
+} from '@hk01-digital/react-native-webview-events/cjs/web'
 import React from 'react'
 
 import Button from '../../components/Button'
+import config from '../../config'
 
-const Menu = () => (
-  <div>
-    <Button text='Testing' />
-  </div>
-)
+class Menu extends React.PureComponent {
+  onBackClick () {
+    if (isSupportedApp(config.supportedAppVersion)) {
+      requestAndWait({
+        type: EVENT_ENUMS.NAVIGATE_GO_BACK
+      })
+    }
+  }
+
+  onLoginClick () {
+    if (isSupportedApp(config.supportedAppVersion)) {
+      requestAndWait({
+        type: EVENT_ENUMS.GET_JWT,
+        payload: {
+          withLogin: true
+        }
+      })
+    }
+  }
+
+  onShareBtnClick () {
+    if (isSupportedApp(config.supportedAppVersion)) {
+      requestAndWait({
+        type: EVENT_ENUMS.OPEN_SHARE_DIALOG,
+        payload: {
+          message: window.location.href,
+          title: 'Greeting from hall',
+          url: window.location.href
+        }
+      })
+    }
+  }
+
+  onHidingHeader () {
+    if (isSupportedApp(config.supportedAppVersion)) {
+      requestAndWait({
+        type: EVENT_ENUMS.SET_NAVIGATION_OPTIONS,
+        payload: {
+          hideHeader: true
+        }
+      })
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <Button text='< Back' onClick={this.onBackClick} />
+        <Button text='Login' onClick={this.onLoginClick} />
+        <Button text='Share' onClick={this.onShareBtnClick} />
+        <Button text='Hide Header' onClick={this.onHidingHeader} />
+      </div>
+    )
+  }
+}
 
 export default Menu
